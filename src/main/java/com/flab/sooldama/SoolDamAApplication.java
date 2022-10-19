d106c8ba129d6828f00bec9dced38b486fa3bb48
sehun-kim <rogarithm@gmail.com>
2022-10-19 21:50:47 +0900

    [#1] 실패하는지 테스트

--- src/main/java/com/flab/sooldama/SoolDamAApplication.java
+++ src/main/java/com/flab/sooldama/SoolDamAApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoolDamAApplication {
     public static void main(String[] args) {
+        System.out.println("hi");
         SpringApplication.run(SoolDamAApplication.class, args);
     }
 }
